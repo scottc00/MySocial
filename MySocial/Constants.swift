2c22862852758b28510189a8bcc0fb76ff2cd854
scottc00 <scottcallery@gmail.com>
2017-08-05 18:09:13 +0100

    Sign in ui custom classes created

---
+++ MySocial/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  MySocial
+//
+//  Created by Scott Callery on 05/08/2017.
+//  Copyright © 2017 Scott Callery. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ MySocial/FancyButton.swift
@@ -0,0 +1,23 @@
+//
+//  FancyButton.swift
+//  MySocial
+//
+//  Created by Scott Callery on 05/08/2017.
+//  Copyright © 2017 Scott Callery. All rights reserved.
+//
+
+import UIKit
+
+class FancyButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        layer.cornerRadius = 2.0
+    }
+
+}
