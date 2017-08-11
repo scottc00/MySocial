0104b08b943b3037660375309c89430f76be1f2c
scottc00 <scottcallery@gmail.com>
2017-08-11 16:00:44 +0100

    completed creating firebaseDB users

---
+++ MySocial/DataService.swift
@@ -0,0 +1,39 @@
+//
+//  DataService.swift
+//  MySocial
+//
+//  Created by Scott Callery on 11/08/2017.
+//  Copyright © 2017 Scott Callery. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+let DB_BASE = Database.database().reference()
+
+class DataService {
+    
+    static let ds = DataService()
+    
+    private var _REF_BASE = DB_BASE
+    private var _REF_POSTS = DB_BASE.child("posts")
+    private var _REF_USERS = DB_BASE.child("users")
+    
+    var REF_BASE: DatabaseReference {
+        return _REF_BASE
+    }
+    
+    var REF_POSTS: DatabaseReference {
+        return _REF_POSTS
+    }
+    
+    var REF_USERS: DatabaseReference {
+        return _REF_USERS
+    }
+    
+    func createFirebaseDBUser(uid: String, userData: Dictionary<String, String>) {
+        REF_USERS.child(uid).updateChildValues(userData)
+    }
+    
+    
+}
--- MySocial/SignInVC.swift
+++ MySocial/SignInVC.swift
@@ -60,7 +60,8 @@ class SignInVC: UIViewController {
             } else {
                 print("Successful sign in with Firebase")
                 if let user = user {
-                    self.completeSignIn(id: user.uid)
+                    let userData = ["provider": credential.provider]
+                    self.completeSignIn(id: user.uid, userData: userData)
                 }
             }
         }
@@ -74,7 +75,8 @@ class SignInVC: UIViewController {
                 if error == nil {
                     print("SCOTT: Email User authenticated with Firebase")
                     if let user = user {
-                        self.completeSignIn(id: user.uid)
+                        let userData = ["provider": user.providerID]
+                        self.completeSignIn(id: user.uid, userData: userData)
                     }
 
                 } else {
@@ -84,7 +86,8 @@ class SignInVC: UIViewController {
                         } else {
                             print("SCOTT: Successfully authenticated with firebase email user")
                             if let user = user {
-                                self.completeSignIn(id: user.uid)
+                                let userData = ["provider": user.providerID]
+                                self.completeSignIn(id: user.uid, userData: userData)
                             }
                         }
                     })
@@ -94,7 +97,8 @@ class SignInVC: UIViewController {
         
     }
     
-    func completeSignIn(id: String) {
+    func completeSignIn(id: String, userData: Dictionary<String, String>) {
+        DataService.ds.createFirebaseDBUser(uid: id, userData: userData)
        let keychainResult = KeychainWrapper.standard.set(id, forKey: KEY_UID)
         print("SCOTT: data saved to keychain \(keychainResult)")
         performSegue(withIdentifier: "goToFeed", sender: nil)
