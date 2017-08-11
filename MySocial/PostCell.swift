26aab762abf368f0b0cbded8ac795971e28eecaa
scottc00 <scottcallery@gmail.com>
2017-08-11 11:57:46 +0100

    Table UI completed

---
+++ MySocial/PostCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostCell.swift
+//  MySocial
+//
+//  Created by Scott Callery on 11/08/2017.
+//  Copyright © 2017 Scott Callery. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likeLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+   
+
+}
