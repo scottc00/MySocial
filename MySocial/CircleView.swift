cc8acd68eead67e59e16b86f673b18128d6927ce
scottc00 <scottcallery@gmail.com>
2017-08-11 11:04:18 +0100

    Designed feed UI

--- MySocial/CircleView.swift
+++ MySocial/CircleView.swift
@@ -1,24 +1,27 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  MySocial
 //
-//  Created by Scott Callery on 05/08/2017.
+//  Created by Scott Callery on 10/08/2017.
 //  Copyright © 2017 Scott Callery. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
-
+class CircleView: UIImageView {
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
         
+        layer.cornerRadius = self.frame.width / 2
     }
 
 }
