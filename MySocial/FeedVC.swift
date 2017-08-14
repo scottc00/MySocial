bba55503ff31e192fe3c1db2ca55bf1071fec87e
scottc00 <scottcallery@gmail.com>
2017-08-14 23:28:02 +0100

    Completed Firebase storage download

--- MySocial/FeedVC.swift
+++ MySocial/FeedVC.swift
@@ -14,12 +14,12 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var imageAdd: CircleView!
-    
-    
+
     
     var posts = [Post]()
     var imagePicker: UIImagePickerController!
-
+    static var imageCache: NSCache<NSString, UIImage> = NSCache()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -65,8 +65,15 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
         let post = posts[indexPath.row]
         
         if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
+            
+
+            if let img = FeedVC.imageCache.object(forKey: post.imageUrl as NSString) {
+                cell.configureCell(post: post, img: img)
+                return cell
+            } else {
             cell.configureCell(post: post)
             return cell
+            }
         } else {
             return PostCell()
         }
